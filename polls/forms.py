4f4a126cf079054dcd5b25af94e38b47c05d7d36
theshahjee <abdullah5mughees@gmail.com>
2024-07-03 14:39:09 +0500

    Initial Commit

---
+++ polls/forms.py
@@ -0,0 +1,13 @@
+# forms.py
+from django import forms
+from .models import Question, Choice
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['question_text']
+
+class ChoiceForm(forms.ModelForm):
+    class Meta:
+        model = Choice
+        fields = ['choice_text']
